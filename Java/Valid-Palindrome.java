5350f893414398d63a89d32b9ef87a3326c50496
Stacklume <safa.syed2786@gmail.com>
2026-08-03 22:49:46 +0530

    Refactor palindrome check for efficiency

--- Java/Valid-Palindrome.java
+++ Java/Valid-Palindrome.java
@@ -1,17 +1,20 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        s=s.toLowerCase().replaceAll("[^a-z0-9]", "");
-        String st="";
-        String rev="";
-        for(int i=0;i<s.length();i++){
-            st +=s.charAt(i);
+       int left=0;
+       int right=s.length()-1;
+       while(left<right){
+        while(left<right && !Character.isLetterOrDigit(s.charAt(left))){
+            left++;
         }
-        for(int j=s.length()-1;j>=0;j--){
-                rev+=s.charAt(j);
-        }
-        if(st.equals(rev)){
-            return true;
-        }
-        return false;
-    }
+        while(left<right && !Character.isLetterOrDigit(s.charAt(right))){
+            right--;
+       }
+       if(Character.toLowerCase(s.charAt(left))!=(Character.toLowerCase(s.charAt(right)))){
+            return false;
+       }
+       left++;
+       right--;
+       }
+        return true;
+}
 }
